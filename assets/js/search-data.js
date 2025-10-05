2bf124ea0702371287ae7f8a75da361024225b25
moulydewan <mouly.dewan@gmail.com>
2025-10-05 00:37:33 +0000

    Deploying to gh-pages from @ moulydewan/moulydewan.github.io@7406a246c6f9cd0f8bfd67c47f148062a7f59e3e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,16 +28,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather.html";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2.html";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-workshop-paper-accepted-at-llm4eval-2025-workshop-at-sigir-25-padova-italy",
+          title: 'Workshop paper accepted at LLM4Eval 2025 Workshop at SIGIR’25 - Padova, Italy!',
           description: "",
           section: "News",},{
       id: 'light-theme',
